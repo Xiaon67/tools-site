667b4f5fabe95aa77cf2c6c578f66e9a1daa4d72
Xiaon67 <xiaon67@users.noreply.github.com>
2026-06-16 15:41:06 +0530

    Update URLs for root domain deployment

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
-const siteUrl = 'https://xiaon67.github.io/tools-site'
+const siteUrl = 'https://xiaon67.github.io'
 
 const pageMeta = {
   '/': {
